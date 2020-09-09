da6568ff477cdf8e15ba1938ebeef5c410733042
kSabchev <kaloyansabchev@gmail.com>
2020-09-09 16:17:00 +0300

    Home

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -15,16 +15,30 @@ function Home() {
                 <div className="home__row">
                     <Product title='The lean startup' price={29,99} 
                     image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg' rating={5}/>
-                    <Product title='The lean startup' price={29,99} 
-                    image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg' rating={3}/>
+                    
+                    <Product id="23445930" title="Amaon Echo (3rd generation) Smart speaker with Alexaa, Charcoal Fabric" price={98.99} rating={5} 
+                    image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"/>
                 </div>
 
                 <div className="home__row">
+                    <Product
+                id="3254354345"
+                        title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+                        price={598.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product
+                        id="90829332"
+                        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+                        price={1094.98}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._SX355_.jpg"/>
                     <Product id="4903850" title="Samsung LC49RG90SSSSUXEN 49' Curved LED Gaming Monitor" price={199.99} rating={3}
                     image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._ACC_SX466_.jpg"/>
-                    <Product id="23445930" title="Amaon Echo (3rd generation) Smart speaker with Alexaa, Charcoal Fabric" price={98.99} rating={5} 
-                    image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"/>
-                    <Product />
                 </div>
 
                 <div className="home__row">
