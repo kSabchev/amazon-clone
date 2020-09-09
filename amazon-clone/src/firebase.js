10a34db1e1f55e52502b126955d6d496b1706a71
kSabchev <kaloyansabchev@gmail.com>
2020-09-09 19:16:34 +0300

    firebase

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,1 +1,9 @@
-//todo
+const firebaseConfig = {
+    apiKey: "AIzaSyBDL-COtKLbIdZfdHP47dKnns7o7aOuLe4",
+    authDomain: "clone-kaloyan.firebaseapp.com",
+    databaseURL: "https://clone-kaloyan.firebaseio.com",
+    projectId: "clone-kaloyan",
+    storageBucket: "clone-kaloyan.appspot.com",
+    messagingSenderId: "613133291744",
+    appId: "1:613133291744:web:4e8763f069cca3da21c085"
+  };
